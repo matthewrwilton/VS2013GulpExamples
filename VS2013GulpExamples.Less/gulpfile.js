20f09180c9ec35e275ffb1195e9204717c8a48e7
Matthew Wilton <matthewrwilton@gmail.com>
2015-05-23 16:02:12 -0500

    Create sourcemaps for compiled CSS.

--- VS2013GulpExamples.Less/gulpfile.js
+++ VS2013GulpExamples.Less/gulpfile.js
@@ -1,17 +1,18 @@
 /// <vs SolutionOpened='watch' />
 
-var gulp = require("gulp");
-var gulpWatch = require("gulp-watch");
-var less = require("gulp-less");
+var gulp = require("gulp"),
+    gulpWatch = require("gulp-watch"),
+    less = require("gulp-less"),
+    sourcemaps = require("gulp-sourcemaps");
 
 gulp.task("less", function () {
     return gulp.src("./Content/Less/Site.less")
+        .pipe(sourcemaps.init())
         .pipe(less())
+        .pipe(sourcemaps.write("../Content"))
         .pipe(gulp.dest("./Content"));
 });
 
 gulp.task("watch", function () {
     gulp.watch(["./Content/Less/*.less"], ["less"]);
 });
-
-gulp.task("default", ["less"], function () { });
